da7d4ba8c148c17f85e38b6448c931eeb93523e4
Sheehan Olver <solver@mac.com>
2021-05-05 15:19:52 +0100

    support padded coefficients (for ∞-dimensional case) (#93)
    * support padded coefficients (for ∞-dimensional case)
    
    * add tests

--- src/bases/bases.jl
+++ src/bases/bases.jl
@@ -122,11 +122,18 @@ copy(L::Ldiv{<:AbstractBasisLayout,BroadcastLayout{typeof(*)},<:Any,<:AbstractQu
 
 # expansion
 _grid(_, P) = error("Overload Grid")
-_grid(lay::ApplyLayout{typeof(*)}, P) = grid(first(arguments(lay,P)))
+
+_mul_grid(_, args) = grid(first(args))
+_mul_grid(::Tuple{Any,PaddedLayout}, (P,c)) = grid(P[:,colsupport(c)])
+
+function _grid(lay::ApplyLayout{typeof(*)}, P)
+    args = arguments(lay,P)
+    _mul_grid(map(MemoryLayout,args), args)
+end
 _grid(::MappedBasisLayout, P) = invmap(parentindices(P)[1])[grid(demap(P))]
 _grid(::SubBasisLayout, P) = grid(parent(P))
 _grid(::WeightedBasisLayouts, P) = grid(unweightedbasis(P))
-grid(P) = _grid(MemoryLayout(typeof(P)), P)
+grid(P) = _grid(MemoryLayout(P), P)
 
 
 struct TransformFactorization{T,Grid,Plan,IPlan} <: Factorization{T}
