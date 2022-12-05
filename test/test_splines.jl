d89df2b686af74a0cac23b7d31c7d243bd21ff94
Sheehan Olver <solver@mac.com>
2022-12-05 20:13:37 +0000

    Update test_splines.jl

--- test/test_splines.jl
+++ test/test_splines.jl
@@ -159,6 +159,7 @@ import ContinuumArrays: basis, AdjointBasisLayout, ExpansionLayout, BasisLayout,
         @test length((D*L).args) == 2
         @test eltype(D*L) == Float64
         @test typeof(diff(L; dims=1)) == typeof(D*L)
+        @test_throws ErrorException diff(L; dims=2)
 
         M = applied(*, (D*L).args..., [1,2,4])
         @test eltype(materialize(M)) == Float64
