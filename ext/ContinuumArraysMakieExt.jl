373eb143482bcedae8a179befd1ac30a4c0bbcd8
Sheehan Olver <solver@mac.com>
2023-08-02 21:41:31 +0100

    Add QuasiPlot for matrices

--- ext/ContinuumArraysMakieExt.jl
+++ ext/ContinuumArraysMakieExt.jl
@@ -1,15 +1,32 @@
 module ContinuumArraysMakieExt
 
-using ContinuumArrays
-using ContinuumArrays: plotgridvalues, AbstractQuasiArray, _split_svec
-import Makie: convert_arguments
+using ContinuumArrays, Makie
+using ContinuumArrays: plotgridvalues, AbstractQuasiArray, AbstractQuasiMatrix, AbstractQuasiVector, _split_svec
+import Makie: convert_arguments, plot!
 
 
-function convert_arguments(g::AbstractQuasiArray)
+function Makie.convert_arguments(p::PointBased, g::AbstractQuasiVector)
     x,v = plotgridvalues(g)
-    tuple(_split_svec(x)..., v)
+    convert_arguments(p, _split_svec(x)..., v)
 end
 
-println("hello")
+@recipe(QuasiPlot, P) do scene
+    Theme(
+    )
+end
+
+Makie.plottype(a::AbstractQuasiMatrix) = QuasiPlot
+
+function Makie.plot!(sc::QuasiPlot)
+    x,v = plotgridvalues(sc[:P][])
+    x2 = _split_svec(x)
+    for j in axes(v,2)
+        lines!(sc, x, v[:,j])
+    end
+    sc
+end
+
+
+
 
 end # module
