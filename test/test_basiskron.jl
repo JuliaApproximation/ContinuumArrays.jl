8c066cba818dd1c384c3430493e12b0b8bf43166
Sheehan Olver <solver@mac.com>
2025-10-13 09:55:53 +0100

    increase coverage

--- test/test_basiskron.jl
+++ test/test_basiskron.jl
@@ -7,5 +7,15 @@ using ContinuumArrays, QuasiArrays, StaticArrays, Test
     xy = axes(K,1)
     f = xy -> ((x,y) = xy; exp(x*cos(y)))
     K \ f.(xy)
+end
 
+@testset "KronExpansion" begin
+    L = LinearSpline(range(0,1; length=4))
+    C = reshape(Vector(1:16), 4, 4)
+    F = L * C * L'
+    @test sum(F) ≈ 8.5
+    @test sum(F; dims=1)[1,0.1] ≈ 3.7
+    @test sum(F; dims=2)[0.1,1] ≈ 7.3
+
+    @test F[0.1,:][0.2] ≈ F[:,0.2][0.1] ≈ F[0.1,0.2]
 end
