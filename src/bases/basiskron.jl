d0e2c0da6859f2c817963e142d3d69d98360bf1c
Sheehan Olver <solver@mac.com>
2025-10-19 10:09:39 +0100

    Better indexing into a KronExpansion

--- src/bases/basiskron.jl
+++ src/bases/basiskron.jl
@@ -9,5 +9,17 @@ is a MemoryLayout corresponding to a quasi-matrix corresponding to the 2D expans
 """
 struct KronExpansionLayout{LayA, LayB} <: AbstractLazyLayout end
 applylayout(::Type{typeof(*)}, ::LayA, ::CoefficientLayouts, ::AdjointBasisLayout{LayB}) where {LayA <: AbstractBasisLayout, LayB <: AbstractBasisLayout} = KronExpansionLayout{LayA,LayB}()
+
 sublayout(::KronExpansionLayout, inds) = sublayout(ApplyLayout{typeof(*)}(), inds)
+sublayout(::KronExpansionLayout{LayA, LayB}, inds::Type{<:Tuple{Inclusion,AbstractVector}}) where {LayA,LayB} = ExpansionLayout{LayA}()
+
+
+sub_basis_layout(::KronExpansionLayout, P, j) = first(arguments(P))
+
+
+function sub_coefficients_layout(::KronExpansionLayout, P, j)
+   _,X,Bt = arguments(P)
+    X * Bt[:,j]
+end
+
 sum_layout(::KronExpansionLayout, F, dims...) = sum_layout(ApplyLayout{typeof(*)}(), F, dims...)
